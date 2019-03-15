c61bd225b4dc5ccd0b53b0902e4e8c0cbe9abebc
TKMAJIZ <karthick.kumar1@wipro.com>
2019-03-15 13:06:15 +0530

    changed few things commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World! node server')
+  response.send('Hello World! node server test')
 })
 
 app.listen(app.get('port'), function() {
